9607cee7f88fc67dcee2ef86f7b637d36704aed2
lzn202 <18858194107@163.com>
2019-04-28 19:29:52 +0800

    1111

--- Warehouse/admin.py
+++ Warehouse/admin.py
@@ -8,32 +8,110 @@ admin = Admin(app=warehouse, name='系统后台', template_mode='bootstrap3')
 
 
 class UserView(ModelView):
+    can_delete = False  # 不能做删除操作
+    # column_display_pk = True  # 显示模型的主键
+    column_display_all_relations = True
+    column_labels = dict(name='姓名', telephone='手机', role='职位', department='部门', create_time='创建时间')  # 命名数据库字段
+    column_exclude_list = ('password', )  # 隐藏的数据库字段
+
+
+class RoleView(ModelView):
     can_delete = False
-    # 命名数据库字段
-    column_labels = dict(
-        name='姓名',
-        telephone='手机',
-        role='角色',
-        create_time='创建时间'
-    )
-    # 隐藏的数据库字段
-    column_exclude_list = (
-        'password'
-    )
+    column_labels = dict(name='职位', remark='备注', create_time='创建时间')
+
+
+class DepartmentView(ModelView):
+    can_delete = False
+    column_labels = dict(name='部门', superior_category='上级部门', creat_time='创建时间')
+
+
+class ClientView(ModelView):
+    can_delete = False
+    column_labels = dict(name='名称', address='地址', contact='联系人', telephone='电话', remark='备注', email='邮件', creat_time='创建时间')
+
+
+class PartnerCategoryView(ModelView):
+    can_delete = False
+    column_display_all_relations = True
+    column_labels = dict(name='名称', remark='备注', creat_time='创建时间', partners='合作商')
+
+
+class PartnerView(ModelView):
+    can_delete = False
+    column_display_all_relations = True
+    column_auto_select_related = True
+    column_labels = dict(name='名称', address='地址', contact='联系人', telephone='电话', remark='备注', email='邮件', category='类型', creat_time='创建时间')
+
+
+class UnitView(ModelView):
+    can_delete = False
+    column_labels = dict(name='名称', creat_time='创建时间')
+
+
+class GoodsCategoryView(ModelView):
+    can_delete = False
+    column_labels = dict(name='名称', remark='备注', creat_time='创建时间')
+
+
+class StandardView(ModelView):
+    can_delete = False
+    column_labels = dict(name='名称', remark='备注', creat_time='创建时间')
+
+
+class GoodsView(ModelView):
+    can_delete = False
+    column_display_all_relations = True
+    column_labels = dict(name='名称', inside_code='内部编号', external_code='外部编号', standard='规格', category='种类', unit='单位', weight='重量', price='价格', remark='备注', creat_time='创建时间')
+
+
+class DepotCategoryView(ModelView):
+    can_delete = False
+    column_labels = dict(name='名称', remark='备注', creat_time='创建时间')
+
+
+class DepotView(ModelView):
+    can_delete = False
+    column_labels = dict(name='名称', depot_category='类型', address='地址', area='面积', contact='联系人', telephone='电话', remark='备注', creat_time='创建时间')
+
+
+class TypeIOView(ModelView):
+    can_delete = False
+    column_labels = dict(name='名称', remark='备注', creat_time='创建时间')
+
+
+class DepositView(ModelView):
+    can_delete = False
+    column_display_pk = True
+    column_labels = dict(id='单号', goods='产品', code='编号', batch='批次', standard='规格', amount='数量', depot='仓库', type_io='出入库类型', partner='合作商', author='制单人', creat_time='制单时间', status='审核状态', check='审核人', check_time='审核时间')
+
+
+class TakeoutView(ModelView):
+    can_delete = False
+    column_display_pk = True
+    column_labels = dict(id='单号', goods='产品', code='编号', batch='批次', standard='规格', amount='数量', depot='仓库', type_io='出入库类型', client='客户', express='物流单号', author='制单人', creat_time='制单时间', status='审核状态', check='审核人', check_time='审核时间')
+
+
+class DamagedView(ModelView):
+    can_delete = False
+    column_display_pk = True
+    column_display_all_relations = True
+
+    column_labels = dict(id='单号', goods='产品', code='编号', batch='批次', standard='规格', amount='数量', depot='仓库', type_io='出入库类型', author='制单人', creat_time='制单时间', status='审核状态', check='审核人', check_time='审核时间')
 
 
 admin.add_view(UserView(User, db.session, name='员工'))
-admin.add_view(ModelView(Role, db.session, name='职位'))
-admin.add_view(ModelView(Department, db.session, name='部门'))
-admin.add_view(ModelView(Client, db.session, name='客户'))
-admin.add_view(ModelView(Partner, db.session, name='合作商'))
-admin.add_view(ModelView(Unit, db.session, name='单位'))
-admin.add_view(ModelView(GoodsCategory, db.session, name='产品种类'))
-admin.add_view(ModelView(Standard, db.session, name='规格'))
-admin.add_view(ModelView(Goods, db.session, name='产品'))
-admin.add_view(ModelView(DepotCategory, db.session, name='仓库类型'))
-admin.add_view(ModelView(Depot, db.session, name='仓库'))
-admin.add_view(ModelView(TypeIO, db.session, name='出入库类型'))
-admin.add_view(ModelView(Deposit, db.session, name='入库'))
-admin.add_view(ModelView(Takeout, db.session, name='出库'))
-admin.add_view(ModelView(Damaged, db.session, name='报损'))
+admin.add_view(RoleView(Role, db.session, name='职位'))
+admin.add_view(DepartmentView(Department, db.session, name='部门'))
+admin.add_view(ClientView(Client, db.session, name='客户'))
+admin.add_view(PartnerCategoryView(PartnerCategory, db.session, name='合作商类型'))
+admin.add_view(PartnerView(Partner, db.session, name='合作商'))
+admin.add_view(UnitView(Unit, db.session, name='单位'))
+admin.add_view(GoodsCategoryView(GoodsCategory, db.session, name='产品种类'))
+admin.add_view(StandardView(Standard, db.session, name='规格'))
+admin.add_view(GoodsView(Goods, db.session, name='产品'))
+admin.add_view(DepotCategoryView(DepotCategory, db.session, name='仓库类型'))
+admin.add_view(DepotView(Depot, db.session, name='仓库'))
+admin.add_view(TypeIOView(TypeIO, db.session, name='出入库类型'))
+admin.add_view(DepositView(Deposit, db.session, name='入库'))
+admin.add_view(TakeoutView(Takeout, db.session, name='出库'))
+admin.add_view(DamagedView(Damaged, db.session, name='报损'))
--- Warehouse/models.py
+++ Warehouse/models.py
@@ -7,7 +7,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 编号
     name = db.Column(db.String(10))  # 姓名
     telephone = db.Column(db.Integer)  # 手机
-    role = db.Column(db.String(20))  # 职位
+    role = db.relationship('Role')  # 职位
+    department = db.relationship('Department')  # 部门
     password = db.Column(db.String(50))  # 密码
     create_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 创建时间
 
@@ -18,6 +19,10 @@ class Role(db.Model):
     name = db.Column(db.String(10))  # 职位名
     remark = db.Column(db.String)  # 备注
     create_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 创建时间
+    user = db.Column(db.String(200), db.ForeignKey('user.id'))  # 对应的外键
+
+    def __repr__(self):
+        return '{}'.format(self.name)
 
 
 # 部门
@@ -26,6 +31,10 @@ class Department(db.Model):
     name = db.Column(db.String(200))  # 名称
     superior_category = db.Column(db.String(200))  # 父类别
     creat_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 创建时间
+    user = db.Column(db.String(200), db.ForeignKey('user.id'))  # 对应的外键
+
+    def __repr__(self):
+        return '{}'.format(self.name)
 
 
 # 客户
@@ -40,6 +49,18 @@ class Client(db.Model):
     creat_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 创建时间
 
 
+# 合作商类型
+class PartnerCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 编号
+    name = db.Column(db.String(200))  # 名称
+    remark = db.Column(db.String())  # 备注
+    creat_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 创建时间
+    partners = db.Column(db.String(200), db.ForeignKey('partner.id'))
+
+    def __repr__(self):
+        return '{}'.format(self.name)
+
+
 # 合作商
 class Partner(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 编号
@@ -49,7 +70,7 @@ class Partner(db.Model):
     telephone = db.Column(db.Integer)  # 电话
     remark = db.Column(db.String)  # 备注
     email = db.Column(db.String(200))  # 邮件
-    category = db.Column(db.String(200))
+    category = db.relationship('PartnerCategory')  # 类型
     creat_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 创建时间
 
 
@@ -59,14 +80,19 @@ class Unit(db.Model):
     name = db.Column(db.String(200))  # 名称
     creat_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 创建时间
 
+    def __repr__(self):
+        return '{}'.format(self.name)
+
 
 # 产品类别
 class GoodsCategory(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 编号
     name = db.Column(db.String(200))  # 名称
-    superior_category = db.Column(db.String(200))  # 父类别
     remark = db.Column(db.String)  # 备注
     creat_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 创建时间
+
+    def __repr__(self):
+        return '{}'.format(self.name)
 
 
 # 产品规格
@@ -76,6 +102,9 @@ class Standard(db.Model):
     remark = db.Column(db.String)  # 备注
     creat_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 创建时间
 
+    def __repr__(self):
+        return '{}'.format(self.name)
+
 
 # 产品管理
 class Goods(db.Model):
@@ -83,14 +112,19 @@ class Goods(db.Model):
     name = db.Column(db.String(200))  # 名称
     inside_code = db.Column(db.String(200))  # 内部编号
     external_code = db.Column(db.String(200))  # 外部编号
-    standard = db.Column(db.String(200), db.ForeignKey('standard.id'))  # 规格（尺码、长度等）
-    category = db.Column(db.String(200), db.ForeignKey('goods_category.id'))  # 种类
-    unit = db.Column(db.String(200), db.ForeignKey('unit.id'))  # 单位
-    package = db.Column(db.String(200))  # 包装类型（袋装、箱装）
+    standard_id = db.Column(db.String(200), db.ForeignKey('standard.id'))
+    standard = db.relationship('Standard')  # 规格（尺码、长度等）
+    category_id = db.Column(db.String(200), db.ForeignKey('good_category.id'))
+    category = db.relationship('GoodsCategory')  # 种类
+    unit_id = db.Column(db.String(200), db.ForeignKey('unit.id'))
+    unit = db.relationship('Unit')  # 单位
     weight = db.Column(db.Integer)  # 重量
     price = db.Column(db.Float)  # 价格
     remark = db.Column(db.String)  # 备注
     creat_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 创建时间
+
+    def __repr__(self):
+        return '{}'.format(self.name)
 
 
 # 仓库类型
@@ -100,12 +134,16 @@ class DepotCategory(db.Model):
     remark = db.Column(db.String)  # 备注
     creat_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 创建时间
 
+    def __repr__(self):
+        return '{}'.format(self.name)
+
 
 # 仓库
 class Depot(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 编号
     name = db.Column(db.String(200))  # 名称
-    category = db.Column(db.String(200), db.ForeignKey('depot_category.id'))  # 仓库类型
+    category_id = db.Column(db.String(200), db.ForeignKey('depot_category.id'))  # 仓库类型
+    depot_category = db.relationship('DepotCategory')
     address = db.Column(db.String(200))  # 地址
     area = db.Column(db.Integer)  # 面积
     contact = db.Column(db.String(200))  # 联系人
@@ -122,57 +160,80 @@ class TypeIO(db.Model):
     remark = db.Column(db.String)  # 备注
     creat_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 创建时间
 
+    def __repr__(self):
+        return '{}'.format(self.name)
+
 
 # 入库管理
 class Deposit(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  #订单号
-    goods = db.Column(db.String(200), db.ForeignKey('goods.id'))  # 产品名
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 订单号
+    goods_id = db.Column(db.String(200), db.ForeignKey('goods.id'))  # 产品名
+    goods = db.relationship('Goods')
     code = db.Column(db.Integer)  # 产品编号
     batch = db.Column(db.Integer)  # 批次
-    standard = db.Column(db.String(200), db.ForeignKey('standard.id'))  # 规格（尺码、长度等）
+    standard_id = db.Column(db.String(200), db.ForeignKey('standard.id'))  # 规格（尺码、长度等）
+    standard = db.relationship('Standard')
     amount = db.Column(db.Integer)  # 数量
-    depot = db.Column(db.String(200), db.ForeignKey('depot.id'))  # 仓库
-    type_io = db.Column(db.String(200), db.ForeignKey('type.id'))  # 出入库类型
-    partner = db.Column(db.String(200), db.ForeignKey('partner.id'))  # 合作商
-    author = db.Column(db.String(200), db.ForeignKey('user.id'))  # 制单人
+    depot_id = db.Column(db.String(200), db.ForeignKey('depot.id'))  # 仓库
+    depot = db.relationship('Depot')
+    type_id = db.Column(db.String(200), db.ForeignKey('type.id'))  # 出入库类型
+    type_io = db.relationship('TypeIO')
+    partner_id = db.Column(db.String(200), db.ForeignKey('partner.id'))  # 合作商
+    partner = db.relationship('Partner')
+    author_id = db.Column(db.String(200), db.ForeignKey('user.id'))  # 制单人
+    author = db.relationship('User')
     creat_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 制单时间
     status = db.Column(db.Boolean, default=False)  # 审核状态
-    check = db.Column(db.String(200), db.ForeignKey('user.id'))  # 审核人
+    check_id = db.Column(db.String(200), db.ForeignKey('user.id'))  # 审核人
+    check = db.relationship('User')
     check_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 审核时间
 
 
 # 出库管理
 class Takeout(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 订单号
-    goods = db.Column(db.String(200), db.ForeignKey('goods.id'))  # 产品名
+    goods_id = db.Column(db.String(200), db.ForeignKey('goods.id'))  # 产品名
+    goods = db.relationship('Goods')
     code = db.Column(db.Integer)  # 产品编号
     batch = db.Column(db.Integer)  # 批次
-    standard = db.Column(db.String(200), db.ForeignKey('standard.id'))  # 规格（尺码、长度等）
+    standard_id = db.Column(db.String(200), db.ForeignKey('standard.id'))  # 规格（尺码、长度等）
+    standard = db.relationship('Standard')
     amount = db.Column(db.Integer)  # 数量
-    depot = db.Column(db.String(200), db.ForeignKey('depot.id'))  # 仓库
-    type_io = db.Column(db.String(200), db.ForeignKey('type.id'))  # 出入库类型
-    client = db.Column(db.String(200), db.ForeignKey('client.id'))  # 客户
+    depot_id = db.Column(db.String(200), db.ForeignKey('depot.id'))  # 仓库
+    depot = db.relationship('Depot')
+    type_id = db.Column(db.String(200), db.ForeignKey('type.id'))  # 出入库类型
+    type_io = db.relationship('TypeIO')
+    client_id = db.Column(db.String(200), db.ForeignKey('client.id'))  # 客户
+    client = db.relationship('Client')
     express = db.Column(db.String(200))  # 物流单号
-    author = db.Column(db.String(200), db.ForeignKey('user.id'))  # 制单人
+    author_id = db.Column(db.String(200), db.ForeignKey('user.id'))  # 制单人
+    author = db.relationship('User')
     creat_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 制单时间
     status = db.Column(db.Boolean, default=False)  # 审核状态
-    check = db.Column(db.String(200), db.ForeignKey('user.id'))  # 审核人
+    check_id = db.Column(db.String(200), db.ForeignKey('user.id'))  # 审核人
+    check = db.relationship('User')
     check_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 审核时间
 
 
 # 报损管理
 class Damaged(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 订单号
-    goods = db.Column(db.String(200), db.ForeignKey('goods.id'))  # 产品名
+    goods_id = db.Column(db.String(200), db.ForeignKey('goods.id'))  # 产品名
+    goods = db.relationship('Goods')
     code = db.Column(db.Integer)  # 产品编号
     batch = db.Column(db.Integer)  # 批次
-    standard = db.Column(db.String(200), db.ForeignKey('standard.id'))  # 规格（尺码、长度等）
+    standard_id = db.Column(db.String(200), db.ForeignKey('standard.id'))  # 规格（尺码、长度等）
+    standard = db.relationship('Standard')
     amount = db.Column(db.Integer)  # 数量
-    depot = db.Column(db.String(200), db.ForeignKey('depot.id'))  # 存入仓库
-    type_io = db.Column(db.String(200), db.ForeignKey('type.id'))  # 出入库（报损）类型
-    author = db.Column(db.String(200), db.ForeignKey('user.id'))  # 制单人
+    depot_id = db.Column(db.String(200), db.ForeignKey('depot.id'))  # 仓库
+    depot = db.relationship('Depot')
+    type_id = db.Column(db.String(200), db.ForeignKey('type.id'))  # 出入库类型
+    type_io = db.relationship('TypeIO')
+    author_id = db.Column(db.String(200), db.ForeignKey('user.id'))  # 制单人
+    author = db.relationship('User')
     creat_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 制单时间
     status = db.Column(db.Boolean, default=False)  # 审核状态
-    check = db.Column(db.String(200), db.ForeignKey('user.id'))  # 审核人
+    check_id = db.Column(db.String(200), db.ForeignKey('user.id'))  # 审核人
+    check = db.relationship('User')
     check_time = db.Column(db.DateTime, default=datetime.datetime.now)  # 审核时间
 
